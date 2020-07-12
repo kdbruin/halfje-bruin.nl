3d86eeb1f42fa0d2bfe37143af16c4eb5ca88cc1
Kees de Bruin <halfje-bruin@gmail.com>
2020-07-12 22:13:02 +0200

    Configuration and deployment updates

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+#
+# Deploy website to actual hosting environment
+#
+# Any argument passed to the script will be added to the rsync command line.
+
+REMOTE_USER=deb4664
+REMOTE_HOST=halfje-bruin.nl
+REMOTE_DIR=domains/${REMOTE_HOST}/public_html
+
+SSH_KEYFILE=${HOME}/.ssh/halfjebruin-ssh
+LOCAL_DIR=./public/
+LOG_DIR=./logs
+EXCLUDES=.rsync-excludes
+
+LOGFILE=${LOG_DIR}/deploy-"$(date '+%Y%m%d-%H%M%S')".log
+
+mkdir -p ${LOG_DIR}
+rm -rf ${LOCAL_DIR}
+
+hugo && \
+rsync "$@"  \
+    --verbose \
+    --archive \
+    --checksum \
+    --chmod=D775,F664 \
+    --no-group \
+    --no-times \
+    --omit-dir-times \
+    --delete-during \
+    --exclude-from=${EXCLUDES} \
+    --log-file=${LOGFILE} \
+    --rsh="ssh -i '${SSH_KEYFILE}'" \
+    ${LOCAL_DIR} \
+    ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
